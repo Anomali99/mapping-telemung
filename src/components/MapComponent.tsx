bb02e4a8240da140ec6b94f3f0ea1321283a9c2a
Anomali99 <fatiqnur@gmail.com>
2025-07-21 23:52:44 +0700

    fix icon marker

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -14,6 +14,16 @@ import L from "leaflet";
 import type { PointType } from "@/data";
 import PointCard from "@/components/PointCard";
 
+import iconUrl from "leaflet/dist/images/marker-icon.png";
+import iconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
+import shadowUrl from "leaflet/dist/images/marker-shadow.png";
+
+L.Icon.Default.mergeOptions({
+    iconUrl,
+    iconRetinaUrl,
+    shadowUrl,
+});
+
 interface MapProps {
     center: [number, number];
     theme?: "default" | "satellite" | "dark";
